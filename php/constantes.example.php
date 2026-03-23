ed403c55bf241cdac1cc8248cb8fa59f55604e9a
samspeed44 <sam44dec@gmail.com>
2026-03-23 17:28:08 +0100

    constante.php added

---
+++ php/constantes.example.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'forum_cir2');
+define('DB_USER', 'VOTRE_USER');
+define('DB_PASS', 'VOTRE_PASSWORD');
+define('DB_CHARSET', 'utf8');
---
+++ php/constants.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'forum_cir2');
+define('DB_USER', 'root');    // à adapter selon ta config
+define('DB_PASS', '');        // à adapter selon ta config
+define('DB_CHARSET', 'utf8');
